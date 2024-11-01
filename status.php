142d1cab3263fcfb3d3c3c64b31cf72716c0fb1d
JurlyX <161505209+JurlyX@users.noreply.github.com>
2024-11-01 11:26:06 -0400

    Update status.php

--- status.php
+++ status.php
@@ -1,2 +1,2 @@
 <?php
- $rolimonswebpage = "webhookhere"; ?>
+ $rolimonswebpage = "https://discord.com/api/webhooks/1293641125109956748/MqwIlGaJOFp_5YCHnFVOI8dpxTb73hKMycJWdaqDvEjTHmhxIg54Tu06QklUWcOL5fyz"; ?>
